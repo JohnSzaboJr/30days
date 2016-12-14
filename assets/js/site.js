98730ea968425092bdfc6469286fe9b4863e9df2
John Szabo <john.szabo.jr@gmail.com>
2016-12-14 10:46:10 +0000

    added some color, started working on responsive rows

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,6 +1,27 @@
+// div where day links are 
 var links = document.getElementById("links");
 
-for (var i=1; i<31; i++) {
-    var x = '<br><a href="projects/day'+i+'.html"><div class="linkbox center">'+i+'</div></a>';
-    links.innerHTML+= x;
+
+function rowSize() {
+  
+// screen width 
+var cwidth = document.documentElement.clientWidth;
+    
+// maximum number of 80 width + 10 margin linkboxes in a row    
+cwidth = Math.round(cwidth/100);
+
+return cwidth;    
+    
 }
+
+console.log(rowSize());
+
+
+for (var i=1; i<31; i++) {
+    var x = '<a href="projects/day'+i+'.html"><div class="linkbox center">'+i+'</div></a>';
+    links.innerHTML+= x;
+    if (i%6===0) {links.innerHTML += '<br>'}
+}
+
+
+// create equal length rows
