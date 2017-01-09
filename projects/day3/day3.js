dd86de45b833aa9aabe3735b82f058d00a3b64cf
John Szabo <john.szabo.jr@gmail.com>
2017-01-09 19:05:03 +0000

    day3 - small update, made handleUpdate function

--- projects/day3/day3.js
+++ projects/day3/day3.js
@@ -2,4 +2,10 @@ document.title="30 Days of Javascript - Day 3";
 
 const inputs = document.querySelectorAll(".controls input");
 
-console.log(inputs);
+function handleUpdate(){
+    console.log(this.value);
+}
+
+inputs.forEach(function(item){
+    item.addEventListener("change", handleUpdate);
+});
