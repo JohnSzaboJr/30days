53ab697224df2eb04eb135219e090a038353bb36
John Szabo <john.szabo.jr@gmail.com>
2017-01-13 20:12:51 +0100

    day 3 complete

--- projects/day3/day3.js
+++ projects/day3/day3.js
@@ -3,9 +3,13 @@ document.title="30 Days of Javascript - Day 3";
 const inputs = document.querySelectorAll(".controls input");
 
 function handleUpdate(){
-    console.log(this.value);
+    const suffix = this.dataset.sizing || "";
+    console.log(this.name);
+    document.documentElement.style.setProperty('--'+this.name, this.value + suffix);
+    
 }
 
 inputs.forEach(function(item){
     item.addEventListener("change", handleUpdate);
+    item.addEventListener("mousemove", handleUpdate);
 });
